69288905b858b878982f70a0dbe31f5103ecbf50
Angelo Chantziantoniou <angelo.chantziantoniou@gov.ab.ca>
2021-09-08 16:21:11 -0600

    initial

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,13 @@
+// sample_spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+describe('My first Test', () => {
+  it('Visits', () => {
+    var url = Cypress.env('URL');
+    cy.log(url);
+    cy.visit(url)
+  })
+})
